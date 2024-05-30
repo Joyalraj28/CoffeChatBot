bd5ceafdd62f78baec2b52e9ea63a3e3befb41ce
Joyalraj28 <76193411+Joyalraj28@users.noreply.github.com>
2024-05-30 08:31:18 +0530

    Update

--- spacy_utils.py
+++ spacy_utils.py
@@ -1,30 +1,44 @@
-import spacy
 import numpy as np
-import subprocess
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
 
-#On time
-# subprocess.run("spacy download en_core_web_sm")
-
-# Load the spaCy model
-nlp = spacy.load("en_core_web_sm")
 
 def tokenize(sentence):
-    doc = nlp(sentence)
-    return [token.text for token in doc]
+    """
+    split sentence into array of words/tokens
+    a token can be a word or punctuation character, or number
+    """
+    return nltk.word_tokenize(sentence)
 
-def lemmatize(word):
-    lemma = nlp(word)[0].lemma_
-    return lemma
 
-def bag_of_words(tokenized_sentence, all_words):
-    lemmatized_sentence = [lemmatize(w) for w in tokenized_sentence]
+def stem(word):
+    """
+    stemming = find the root form of the word
+    examples:
+    words = ["organize", "organizes", "organizing"]
+    words = [stem(w) for w in words]
+    -> ["organ", "organ", "organ"]
+    """
+    return stemmer.stem(word.lower())
 
-    bag = np.zeros(len(all_words), dtype=np.float32)
-    for idx, w in enumerate(all_words):
-        if w in lemmatized_sentence:
+
+def bag_of_words(tokenized_sentence, words):
+    """
+    return bag of words array:
+    1 for each known word that exists in the sentence, 0 otherwise
+    example:
+    sentence = ["hello", "how", "are", "you"]
+    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    """
+    # stem each word
+    sentence_words = [stem(word) for word in tokenized_sentence]
+    # initialize bag with 0 for each word
+    bag = np.zeros(len(words), dtype=np.float32)
+    for idx, w in enumerate(words):
+        if w in sentence_words: 
             bag[idx] = 1
 
     return bag
-
-
-# print(tokenize("Hello this is running like a bull."))
